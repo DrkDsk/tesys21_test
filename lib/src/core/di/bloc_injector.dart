8307a526def1ad1a2f60c27e5f6c092282afcb05
Alfredo Palacios <45542462+DrkDsk@users.noreply.github.com>
2025-07-29 08:49:46 -0600

    feat: dependencies injected

--- lib/src/core/di/bloc_injector.dart
+++ lib/src/core/di/bloc_injector.dart
@@ -5,6 +5,11 @@ import 'package:tesys21_test/src/features/pokemon_list/data/repositories/pokemon
 import 'package:tesys21_test/src/features/pokemon_list/domain/datasources/pokemon_list_datasource.dart';
 import 'package:tesys21_test/src/features/pokemon_list/domain/repositories/pokemon_list_repository.dart';
 import 'package:tesys21_test/src/features/pokemon_list/presentation/blocs/pokemon_list_bloc.dart';
+import 'package:tesys21_test/src/features/show_pokemon/data/datasources/pokemon_show_datasource_dio_impl.dart';
+import 'package:tesys21_test/src/features/show_pokemon/data/repositories/pokemon_show_repository_impl.dart';
+import 'package:tesys21_test/src/features/show_pokemon/domain/datasources/pokemon_show_datasource.dart';
+import 'package:tesys21_test/src/features/show_pokemon/domain/repositories/pokemon_show_repository.dart';
+import 'package:tesys21_test/src/features/show_pokemon/presentation/blocs/pokemon_show_bloc.dart';
 
 final getIt = GetIt.instance;
 
@@ -14,11 +19,14 @@ Future<void> initBlocDependencies() async {
   getIt.registerSingleton(dioClient);
 
   //DataSources
-  getIt.registerLazySingleton<PokemonListDataSource>(() => PokemonListDataSourceDioImpl(dio: dioClient.dio));
+  getIt.registerLazySingleton<PokemonListDataSource>(() => PokemonListDataSourceDioImpl(dio: getIt<DioClient>().dio));
+  getIt.registerLazySingleton<PokemonShowDataSource>(() => PokemonShowDataSourceDioImpl(dio: getIt<DioClient>().dio));
 
   //repositories
   getIt.registerLazySingleton<PokemonListRepository>(() => PokemonListRepositoryImpl(dataSource: getIt<PokemonListDataSource>()));
+  getIt.registerLazySingleton<PokemonShowRepository>(() => PokemonShowRepositoryImpl(dataSource: getIt<PokemonShowDataSource>()));
 
   //blocs
   getIt.registerFactory<PokemonListBloc>(() => PokemonListBloc(repository: getIt<PokemonListRepository>()));
+  getIt.registerFactory<PokemonShowBloc>(() => PokemonShowBloc(repository: getIt<PokemonShowRepository>()));
 }
